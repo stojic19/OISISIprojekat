d2f5713ade54234f7f76b6911bc1622649e5b7c8
Magdalena Reljin <reljin.ra82.2018@uns.ac.rs>
2020-12-03 11:10:37 +0100

    #glavni_prozor Dodavanje pocetnog prozora

---
+++ src/izgled/MainFrame.java
@@ -0,0 +1,30 @@
+package izgled;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MainFrame extends JFrame{
+	public MainFrame(){
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        int screenHeight = screenSize.height;
+        int screenWidth = screenSize.width;
+        setSize(3*screenWidth / 4, 3*screenHeight / 4);
+        setTitle("Studentska služba");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        
+        JPanel panel = new JPanel();
+        panel.setBackground(new Color(155,172,194));
+        this.add(panel);
+        
+        JLabel lbl = new JLabel("TO DO:Prikaz entiteta sistema");
+        panel.add(lbl);
+        
+	}
+}
--- src/studentska/Main.java
+++ src/studentska/Main.java
@@ -1,10 +1,13 @@
 package studentska;
 
+import izgled.MainFrame;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		MainFrame mf=new MainFrame();
+		mf.setVisible(true);
 	}
 
 }
