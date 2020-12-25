6e21a7bae38f835f05159bceafa11667bacf716b
stojic19 <aleksa.stojic19@uns.ac.rs>
2020-12-25 14:39:28 +0100

    #ocena Dodat model ocena

---
+++ src/model/Ocena.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.util.Date;
+
+public class Ocena {
+
+	public enum VrednostOcene
+	{
+		SEST, SEDAM, OSAM, DEVET, DESET;
+	}
+	
+	//private Student student;
+	private Predmet predmet;
+	private VrednostOcene vrednostOcene;
+	private Date datumPolaganja;
+	
+	public Ocena(/*Student student,*/Predmet predmet,VrednostOcene ocena,Date datPol)
+	{
+		//setStudent(student);
+		setPredmet(predmet);
+		setVrednostOcene(vrednostOcene);
+		setDatumPolaganja(datPol);
+	}
+	/*public Student getStudent() {
+		return student;
+	}
+	public void setStudent(Student student) {
+		this.student = student;
+	}*/
+	public Predmet getPredmet() {
+		return predmet;
+	}
+	public void setPredmet(Predmet predmet) {
+		this.predmet = predmet;
+	}
+	public VrednostOcene getVrednostOcene() {
+		return vrednostOcene;
+	}
+	public void setVrednostOcene(VrednostOcene vrednostOcene) {
+		this.vrednostOcene = vrednostOcene;
+	}
+	public Date getDatumPolaganja() {
+		return datumPolaganja;
+	}
+	public void setDatumPolaganja(Date datumPolaganja) {
+		this.datumPolaganja = datumPolaganja;
+	}
+	
+}
