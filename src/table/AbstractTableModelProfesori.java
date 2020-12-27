32f1ac2f73ff44697d97062a82c5e72320d661fa
Magdalena Reljin <reljin.ra82.2018@uns.ac.rs>
2020-12-27 23:36:42 +0100

    #toolbar file separator

--- src/table/AbstractTableModelProfesori.java
+++ src/table/AbstractTableModelProfesori.java
@@ -7,6 +7,7 @@ import javax.swing.table.AbstractTableModel;
 import model.BazaProfesora;
 
 public class AbstractTableModelProfesori extends AbstractTableModel{
+	
 	/**
 	 * 
 	 */
--- src/table/ProfesoriJTable.java
+++ src/table/ProfesoriJTable.java
@@ -27,6 +27,7 @@ public class ProfesoriJTable extends JTable {
         this.setRowSelectionAllowed(true);
         this.setColumnSelectionAllowed(true);
         this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
         this.getTableHeader().setBackground(new Color(170, 167, 196));
         this.getTableHeader().setFont(this.getTableHeader().getFont().deriveFont(Font.BOLD));
         this.setModel(new AbstractTableModelProfesori());
@@ -48,7 +49,6 @@ public class ProfesoriJTable extends JTable {
             if(row%2==0) {
             	c.setBackground(Color.LIGHT_GRAY);
             }else {
-            	
             	c.setBackground(new Color(170, 167, 196));
             }
         }
